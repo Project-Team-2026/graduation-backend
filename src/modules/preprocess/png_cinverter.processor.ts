8a53074810c0c217cfb7286bf8694e752d83eaec
ZiadSamirNassar <ziadnassar565@gmail.com>
2026-03-08 22:22:58 +0200

    post png convert jop complet add preprocessing jop

--- src/modules/preprocess/png_cinverter.processor.ts
+++ src/modules/preprocess/png_cinverter.processor.ts
@@ -1,15 +1,18 @@
 
 import { ProcessingStatus, Tasks } from '@/common';
-import { Processor, Process } from '@nestjs/bull';
-import type { Job } from 'bull';
-import { AnswerSheetRepository } from '@models/index';
 import { runPythonScript } from '@/utils';
+import { AnswerSheetRepository } from '@models/index';
+import { Process, Processor, InjectQueue } from '@nestjs/bull';
+import type { Job, Queue } from 'bull';
 import { Types } from 'mongoose';
 
 @Processor(Tasks.PNG_CONVERTER)
 export class PngCinverterProcessor {
 
-    constructor( private readonly answerSheetRepository: AnswerSheetRepository) {
+    constructor( 
+        private readonly answerSheetRepository: AnswerSheetRepository,
+        @InjectQueue(Tasks.PREPROCESS) private readonly preprocessingQueue: Queue
+    ) {
         
     }
 
@@ -36,14 +39,45 @@ export class PngCinverterProcessor {
             processingStatus: ProcessingStatus.PROCESSING
         });
 
+        // after converting all pages to png, send job to preprocessing processor 
+        // (add job to preprocessing queue)
+        this.preprocessingQueue.add(
+            'run-preprocessing',
+            {
+                answerSheetId: answerSheetId,
+                filePath: result.data[0]
+            },
+            {
+                attempts: 3,
+                backoff: 5000
+            }
+        )
+
         // if pdf has multiple pages, create multiple answer sheets
         if (result.data.length > 1) {
             for (let i = 1; i < result.data.length; i++) {
+
+                // create new answer sheet for each page
                 await this.answerSheetRepository.create({
                     examId: new Types.ObjectId(examId),
                     filePath: result.data[i],
                     processingStatus: ProcessingStatus.PROCESSING
                 });
+
+
+                // after converting all pages to png, send job to preprocessing processor 
+                // (add job to preprocessing queue)
+                this.preprocessingQueue.add(
+                    'run-preprocessing',
+                    {
+                        answerSheetId: answerSheetId,
+                        filePath: result.data[i]
+                    },
+                    {
+                        attempts: 3,
+                        backoff: 5000
+                    }
+                )
             }
         }
 
